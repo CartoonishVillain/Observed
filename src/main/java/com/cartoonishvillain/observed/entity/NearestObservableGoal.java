042d36f7b556b78e8114a9e41fe80210c6586905
Cartoonish Villain <jedijoe93@gmail.com>
2021-10-05 14:01:54 -0400

    Reduce reused code on targeting

--- src/main/java/com/cartoonishvillain/observed/entity/NearestObservableGoal.java
+++ src/main/java/com/cartoonishvillain/observed/entity/NearestObservableGoal.java
@@ -11,6 +11,6 @@ import java.util.function.Predicate;
 public class NearestObservableGoal extends NearestAttackableTargetGoal {
     public NearestObservableGoal(Mob p_26053_, Class p_26054_, int p_26055_, boolean p_26056_, boolean p_26057_, @Nullable Predicate<LivingEntity> p_26058_) {
         super(p_26053_, p_26054_, p_26055_, p_26056_, p_26057_, p_26058_);
-        this.targetConditions = TargetingConditions.forCombat().range(this.getFollowDistance()).selector(p_26058_).ignoreLineOfSight();
+        this.targetConditions = this.targetConditions.ignoreLineOfSight();
     }
 }
