5e9975916213245dce660729f5ec0dce22222f90
Cartoonish Villain <jedijoe93@gmail.com>
2021-10-05 19:06:12 -0400

    Finishes Observer's base behavior

--- src/main/java/com/cartoonishvillain/observed/entity/ObserverEntity.java
+++ src/main/java/com/cartoonishvillain/observed/entity/ObserverEntity.java
@@ -1,19 +1,20 @@
 package com.cartoonishvillain.observed.entity;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.AreaEffectCloud;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.RangedAttackGoal;
-import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.RangedAttackMob;
-import net.minecraft.world.entity.monster.Spider;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
@@ -23,16 +24,28 @@ public class ObserverEntity extends Monster implements RangedAttackMob {
         super(p_33002_, p_33003_);
     }
 
+    protected Player target;
+    protected BlockPos lastLoc;
+
+    protected int PanicTicks = 0;
+
+
     public static AttributeSupplier.Builder customAttributes(){
         return Mob.createMobAttributes()
-                .add(Attributes.KNOCKBACK_RESISTANCE, 20D).add(Attributes.MAX_HEALTH, 20D).add(Attributes.MOVEMENT_SPEED, 0.5d).add(Attributes.ARMOR, 5);
+                .add(Attributes.MAX_HEALTH, 20D).add(Attributes.MOVEMENT_SPEED, 0.4d).add(Attributes.ARMOR, 5);
     }
 
     @Override
     protected void registerGoals() {
         super.registerGoals();
-        this.goalSelector.addGoal(1, new ObservationGoal(this, 1.25D, 20, 20));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Player.class, 10f, 1.0D, 1.2D, this::avoid ));
+        this.goalSelector.addGoal(2, new ObservationGoal(this, 1.25D, 20, 20));
+        this.goalSelector.addGoal(3, new ObserverMovementGoal<>(this));
         this.targetSelector.addGoal(1, new NearestObservableGoal(this, Player.class, 16, false, false, this::shouldAttack));
+    }
+
+    private boolean avoid(@Nullable LivingEntity entity){
+        return PanicTicks > 0;
     }
 
     public boolean shouldAttack(@Nullable LivingEntity entity){
@@ -44,6 +57,44 @@ public class ObserverEntity extends Monster implements RangedAttackMob {
         if(p_33317_ instanceof Player) {
             affectPlayer((Player) p_33317_);
         }
+    }
+
+    @Override
+    public void tick() {
+        if(hasEffect(MobEffects.POISON)) removeEffect(MobEffects.POISON);
+        super.tick();
+        if(getTarget() != null && getTarget() instanceof Player) target = (Player) getTarget();
+
+        if(getTarget() == null && target != null) {lastLoc = target.getOnPos(); target = null;}
+
+        if(getTarget() != null && lastLoc != null) resetLastLoc();
+
+        if(PanicTicks > 0) PanicTicks--;
+
+    }
+
+    @Override
+    public boolean hurt(DamageSource p_21016_, float p_21017_) {
+
+        if(!this.level.isClientSide() && PanicTicks < 20) {
+            AreaEffectCloud areaEffectCloud = new AreaEffectCloud(EntityType.AREA_EFFECT_CLOUD, this.level);
+            areaEffectCloud.setPotion(Potions.STRONG_POISON);
+            areaEffectCloud.setDuration(80);
+            areaEffectCloud.setRadius(3);
+            areaEffectCloud.setRadiusPerTick(0.05f);
+            areaEffectCloud.moveTo(this.getX(), this.getY(), this.getZ());
+            this.level.addFreshEntity(areaEffectCloud);
+        }
+        PanicTicks = 100;
+        return super.hurt(p_21016_, p_21017_);
+    }
+
+    public BlockPos getLastLoc(){
+        return lastLoc;
+    }
+
+    public void resetLastLoc(){
+        lastLoc = null;
     }
 
     private void affectPlayer(Player player){
---
+++ src/main/java/com/cartoonishvillain/observed/entity/ObserverMovementGoal.java
@@ -0,0 +1,46 @@
+package com.cartoonishvillain.observed.entity;
+
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.util.DefaultRandomPos;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.EnumSet;
+
+//based on move towards raid goal
+
+
+public class ObserverMovementGoal<T extends ObserverEntity> extends Goal {
+    private final T observer;
+
+    public ObserverMovementGoal(T observer){
+        this.observer = observer;
+        this.setFlags(EnumSet.of(Flag.MOVE));
+    }
+
+    @Override
+    public boolean canUse() {
+        return this.observer.getLastLoc() != null && this.observer.getTarget() == null;
+    }
+
+    @Override
+    public boolean canContinueToUse() {
+        return this.observer.getLastLoc() != null && this.observer.getTarget() == null;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (!this.observer.isPathFinding()) {
+            Vec3 vector3d = DefaultRandomPos.getPosTowards(this.observer, 15, 4, Vec3.atBottomCenterOf(observer.getLastLoc()), (double)((float)Math.PI / 10F));
+            if (vector3d != null) {
+                this.observer.getNavigation().moveTo(vector3d.x, vector3d.y, vector3d.z, 1D);
+            }
+
+            if(this.observer.getNavigation().getPath() != null) {
+                if (this.observer.getNavigation().getPath().getDistToTarget() < 2) {
+                    this.observer.resetLastLoc();
+                }
+            }
+        }
+    }
+}
