73490bc43ad8aa1ce38367c80fd1f2e9fe04be26
Cartoonish Villain <jedijoe93@gmail.com>
2021-10-06 00:35:33 -0400

    Added potion effect for later

---
+++ src/main/java/com/cartoonishvillain/observed/ObserveEffect.java
@@ -0,0 +1,26 @@
+package com.cartoonishvillain.observed;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ObserveEffect {
+    public static MobEffect observed;
+
+    public static void init(){
+        observed = new ModdedPotionEffects( MobEffectCategory.HARMFUL, 9606036, new ResourceLocation(Observed.MODID, "observedeffect"));
+    }
+
+    /*
+        While code reusage is minimal would like to shout out the immersive engineering team and BluSunrize for having such a neat license to help me get through this bit in particular
+     */
+    public static class ModdedPotionEffects extends MobEffect {
+
+        protected ModdedPotionEffects(MobEffectCategory p_19451_, int p_19452_, ResourceLocation location) {
+            super(p_19451_, p_19452_);
+            ForgeRegistries.MOB_EFFECTS.register(this.setRegistryName(location));
+        }
+    }
+}
--- src/main/java/com/cartoonishvillain/observed/events/ModBusEvents.java
+++ src/main/java/com/cartoonishvillain/observed/events/ModBusEvents.java
@@ -1,8 +1,11 @@
 package com.cartoonishvillain.observed.events;
 
+import com.cartoonishvillain.observed.ObserveEffect;
 import com.cartoonishvillain.observed.Observed;
 import com.cartoonishvillain.observed.Register;
 import com.cartoonishvillain.observed.entity.ObserverEntity;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,5 +15,10 @@ public class ModBusEvents {
     @SubscribeEvent
     public static void Attributes(final EntityAttributeCreationEvent event){
         event.put(Register.OBSERVER.get(), ObserverEntity.customAttributes().build());
+    }
+
+    @SubscribeEvent
+    public static void effect(final RegistryEvent.Register<MobEffect> event){
+        ObserveEffect.init();
     }
 }
