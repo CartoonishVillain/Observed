d76404880bdbcdc3017e56429aff05b6fa4cabab
Cartoonish Villain <jedijoe93@gmail.com>
2021-10-11 10:49:25 -0400

    Spawn rules updated

--- src/main/java/com/cartoonishvillain/observed/events/Spawns.java
+++ src/main/java/com/cartoonishvillain/observed/events/Spawns.java
@@ -2,15 +2,17 @@ package com.cartoonishvillain.observed.events;
 
 import com.cartoonishvillain.observed.Observed;
 import com.cartoonishvillain.observed.Register;
-import net.minecraft.entity.EntityClassification;
-import net.minecraft.entity.EntitySpawnPlacementRegistry;
-import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.Random;
 
 @Mod.EventBusSubscriber(modid = Observed.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class Spawns {
@@ -28,7 +30,11 @@ public class Spawns {
             EntitySpawnPlacementRegistry.register(Register.OBSERVER.get(), EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, MobEntity::checkMobSpawnRules);
         }
         else
-        EntitySpawnPlacementRegistry.register(Register.OBSERVER.get(), EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, Heightmap.Type.WORLD_SURFACE, MobEntity::checkMobSpawnRules);
+        EntitySpawnPlacementRegistry.register(Register.OBSERVER.get(), EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, Spawns::spawnRules);
 
+    }
+
+    public static boolean spawnRules(EntityType<? extends MobEntity> p_223315_0_, IWorld p_223315_1_, SpawnReason p_223315_2_, BlockPos p_223315_3_, Random p_223315_4_){
+        return p_223315_1_.canSeeSky(p_223315_3_) && MobEntity.checkMobSpawnRules(p_223315_0_, p_223315_1_, p_223315_2_, p_223315_3_, p_223315_4_);
     }
 }
